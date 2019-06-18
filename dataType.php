b83d9c6b91db74c5f7c6d30da8a66666125d62c8
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-06-17 18:44:25 -0700

    Update dataType.php

--- dataType.php
+++ dataType.php
@@ -3,9 +3,13 @@
 	echo $total;
 	echo "<br />";
 	echo gettype($total);
-  echo "<br />";
+	echo "<br />";
 	$total = "Mary";
 	echo $total;
-  echo "<br />";
+	echo "<br />";
 	echo gettype($total);
+	echo "<br />";
+	echo "This is a test"."<br />";
+	$x = var_dump($total);
+	echo $x;
 ?>
