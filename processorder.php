f931e58b9915078aa2aab4be7e4a445250baf85b
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-06-19 18:28:16 -0700

    Update processorder.php

--- processorder.php
+++ processorder.php
@@ -6,5 +6,12 @@
 	<body>
 		<h1>Ghazaleh's Auto Parts</h1>
 		<h2>Order Results</h2>
+		<?php
+			/*	Author: Krista Ghazaleh
+				Last Modified: 6/19/2019
+				This script will process the customer order
+			*/
+			echo '<p>Order processed.</p>';
+		?>
 	</body>
 </html>
