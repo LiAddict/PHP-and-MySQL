97ce5b179eef6af5124b3b04e8082234aa0ce867
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-07-03 18:19:48 -0700

    Add files via upload

---
+++ front_page.php
@@ -0,0 +1,33 @@
+<?php
+	$pictures =array('brakes.png', 'headlight.png', 'spark_plug.png',
+			'steering_wheel.png', 'tire.png',
+			'wiper_blade.png');
+	shuffle($pictures);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ghazaleh's Auto Parts</title>
+</head>
+<body>
+	<!-- page header -->
+	<header>
+		<h1><center>Ghazaleh's Auto Parts</center></h1>
+	</header>
+	
+	<div align="center">
+		<table width="75%">
+			<tr>
+				<?php
+					for ($i = 0; $i < 3; $i++) {
+						echo "<td style=\"width: 25%; text-align: center\">
+							<img src=\"photos\\";
+							echo $pictures[$i];
+							echo "\" height=\"75\" width=\"75\"/></td>";
+					}
+				?>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
