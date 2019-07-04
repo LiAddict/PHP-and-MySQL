012abe5b2aec672a8552a1331e6004b7f1381489
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-07-03 17:50:06 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Krista's Auto Parts</title>
+	<link href="styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<!-- Page Header -->
+<header>
+	<center><h1>Krista's Auto Parts</h1></center>
+</header>
+
+<!-- Menu -->
+<nav>
+	<div class="menuitem">
+		<a href="front_page.php">
+			<img src="side-logo.gif" alt="" height="20" width="20" />
+			<span class="menutext">Order Form</span>
+		</a>
+	</div>
+	
+	<div class="menuitem">
+		<a href="feedback.php">
