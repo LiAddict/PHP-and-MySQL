3b1a88b56abf9ab8506c2f11b0ac673e822110b2
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-07-03 18:04:49 -0700

    Update header.php

--- header.php
+++ header.php
@@ -15,9 +15,28 @@
 	<div class="menuitem">
 		<a href="front_page.php">
 			<img src="side-logo.gif" alt="" height="20" width="20" />
+			<span class="menutext">Home</span>
+		</a>
+	</div>
+	
+	<div class="menuitem">
+		<a href="orderform.php">
+			<img src="side-logo.gif" alt="" height="20" width="20" />
 			<span class="menutext">Order Form</span>
 		</a>
 	</div>
 	
 	<div class="menuitem">
 		<a href="feedback.php">
+			<img src="side-logo.gif" alt="" height="20" width="20" />
+			<span class="menutext">Feedback</span>
+		</a>
+	</div>
+	
+	<div class="menuitem">
+		<a href="vieworders.php">
+			<img src="side-logo.gif" alt="" height="20" width="20" />
+			<span class="menutext">View Order</span>
+		</a>
+	</div>
+</nav>
