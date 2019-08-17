bee68575f93540d39441a12e4f26b661a59627ab
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2019-08-17 12:09:40 -0700

    Add files via upload

---
+++ locations.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+	<title>Locations</title>
+	<meta charset=utf-8>
+	<link rel="stylesheet" type="text/css" href="whales.css">
+	<style type="text/css">
+		table {
+			width: 200px;
+			background:white;
+			color: black;
+			border: 1px black solid;
+			border-collapse: collapse;
+			margin: auto;
+		}
+		td {
+			border: 1px black solid;
+			padding: 1px 0 1px 4 px;
+			text-align: left;
+		}
+	</style>
+</head>
+<body>
+	<div id="container">
+		<!--Add include statements here-->
+		<?php include("includes/header.php"); ?>
+		<?php include("includes/nav.php"); ?>
+		
+		<div id="midcol">
+				<h2>Locations Table Information</h2>
+				<p>
+					<!--Add the table here-->
+					<?php
+					
+					?>
+				</p>
+			</div>
+	</div>
+	<!--Add footer here-->
+	<?php include("includes/footer.php"); ?>
+</body>
+</html>
---
+++ mysqli_connect.php
@@ -0,0 +1,14 @@
+<?php
+	//This creates a connection to the whalesdb database
+	//Set the access details as constants:
+	DEFINE ('DB_USER', 'kristaghazaleh');
+	DEFINE ('DB_PASSWORD', 'Jaredworkdog0518');
+	DEFINE ('DB_HOST', 'localhost');
+	DEFINE ('DB_NAME', 'whales_db');
+	//Make the connection:
+	$dbcon = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die
+		('could not connect to MySQL: ' . mysqli_connect_error() );
+	//Set the encoding
+	mysqli_set_charset($dbcon, 'utf8');
+	?>
+	
